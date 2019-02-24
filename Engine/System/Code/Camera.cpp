640c4955389bf415047ae805e05f48a85773e6dc
KimMolang <bluevill04@gmail.com>
2019-02-25 03:32:24 +0900

    Modify Camera::SetWorldMatrix

--- Engine/System/Code/Camera.cpp
+++ Engine/System/Code/Camera.cpp
@@ -42,9 +42,11 @@ void Camera::SetWorldMatrix
 	, const D3DXVECTOR3& _vScale /*= D3DXVECTOR3(1.0f, 1.0f, 1.0f)*/
 )
 {
-	m_vEye = _vPos;
-	m_vAt = m_vEye;
-	m_vAt.z += 1.0f;
+	// _vRadian;
+
+	const D3DXVECTOR3 minusValue = m_vEye - _vPos;
+	m_vEye -= minusValue;
+	m_vAt -= minusValue;
 }
 
 void Camera::Init_ViewBuffer()
